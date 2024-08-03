3cd57a649aa3cdde0ac5dda9a83e00ba0024b5ec
apple <apple@192.168.174.13>
2024-08-03 23:07:33 +0530

    hello

--- src/polymorphism/Animal.java
+++ src/polymorphism/Animal.java
@@ -1,5 +1,33 @@
 package polymorphism;
+class Animal {
+    // Method to be overridden
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
 
-public class Animal {
+class Dog extends Animal {
+    // Overriding the sound method
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
 
+class Cat extends Animal {
+    // Overriding the sound method
+    @Override
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog(); // Animal reference but Dog object
+        Animal myCat = new Cat(); // Animal reference but Cat object
+
+        myDog.sound(); // Output: Dog barks
+        myCat.sound(); // Output: Cat meows
+    }
 }
--- src/polymorphism/MathOperation.java
+++ src/polymorphism/MathOperation.java
@@ -1,10 +1,26 @@
 package polymorphism;
+class MathOperation {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-public class MathOperation {
+    // Overloaded method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Overloaded method to add two double values
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
 
-	}
-
+public class Main {
+    public static void main(String[] args) {
+        MathOperation operation = new MathOperation();
+        System.out.println(operation.add(2, 3)); // Output: 5
+        System.out.println(operation.add(2, 3, 4)); // Output: 9
+        System.out.println(operation.add(2.5, 3.5)); // Output: 6.0
+    }
 }
